e8c3693a6ca31900ec6f7355ba0257f650006243
hajidalakhtar <hajidalakhtar@gmail.com>
2020-07-21 09:37:28 +0700

    Update Stock.php

--- app/Stock.php
+++ app/Stock.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
+    
+    use Notifiable;
+
     protected $table = 'Stock';
     protected $connection = 'mysql_stock';
     public $timestamps = false;
